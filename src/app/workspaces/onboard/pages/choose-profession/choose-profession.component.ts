cbde1c8feb19878c1ac69656912055805415899c
Vamsi Krishna Kodimela <vamsikrishnakdml@gmail.com>
2023-05-15 22:57:14 +0530

    Design Choose Your Profession

--- src/app/workspaces/onboard/pages/choose-profession/choose-profession.component.ts
+++ src/app/workspaces/onboard/pages/choose-profession/choose-profession.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-choose-profession',
   templateUrl: './choose-profession.component.html',
-  styleUrls: ['./choose-profession.component.scss']
+  styleUrls: ['./choose-profession.component.scss'],
 })
 export class ChooseProfessionComponent {
+  selectedProfession: string = '';
+  professions: Profession[] = [
+    {
+      id: 'developer',
+      title: 'Developer',
+      image: 'developer.svg',
+    },
+    {
+      id: 'designer',
+      title: 'Designer',
+      image: 'designer.svg',
+    },
+    {
+      id: 'other',
+      title: 'Other',
+      image: 'other.svg',
+    },
+  ];
+  getProfessionImage(profession: Profession): string {
+    return `url(/assets/images/professions/${profession.image})`;
+  }
+  selectProfession(profession: Profession): void {
+    this.selectedProfession = profession.id;
+  }
+}
 
+interface Profession {
+  title: string;
+  id: string;
+  image: string;
 }
