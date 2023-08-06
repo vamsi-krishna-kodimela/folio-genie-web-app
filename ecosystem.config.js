00e6e31386c2a2e4a5f01b4731068270fd7c1360
vamsi-krishna-kodimela <vamsikrishnakdml@gmail.com>
2023-08-06 21:29:53 +0530

    Updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "FolioGenie Web App",
-      script: "http-server /dist/folio-genie-web-app -p 4200 -P http://localhost:4200?",
+      script: "http-server ./dist/folio-genie-web-app -p 4200 -P http://localhost:4200?",
     },
   ],
 };
