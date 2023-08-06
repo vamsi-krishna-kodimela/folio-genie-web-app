c954e4481e4a261f9892fb0faafaca7540b82d8c
vamsi-krishna-kodimela <vamsikrishnakdml@gmail.com>
2023-08-06 21:47:45 +0530

    Updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,14 @@ module.exports = {
   apps: [
     {
       name: "FolioGenie Web App",
-      script: "serve 4200 --spa",
+      script: "serve",
+      env: {
+        NODE_ENV: "production",
+        PM2_SERVE_PATH: './dist/folio-genie-web-app',
+        PM2_SERVE_PORT: 4200,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: './index.html'
+    },
     },
   ],
 };
